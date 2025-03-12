cfb6746cd7238221353df36c1a508ec8bf3891e2
HammadAli132 <108983041+HammadAli132@users.noreply.github.com>
2025-03-12 22:02:29 +0500

    Navigation Bar Complete

--- Frontend/src/components/NavigationBar.jsx
+++ Frontend/src/components/NavigationBar.jsx
@@ -1,20 +1,48 @@
 import React from 'react';
 import avatar from '../assets/avatar-659651_640.png'
+import { BellDot, CircleEllipsis, Kanban, LayoutDashboard, ListTodo } from 'lucide-react';
+
+const Icons = [
+    LayoutDashboard,
+    ListTodo,
+    Kanban,
+    BellDot,
+    CircleEllipsis
+];
 
 export default function NavigationBar() {
+    const [activeIcon, setActiveIcon] = React.useState(0);
+
     return (
         <div className="py-3 lg:flex-shrink-0 lg:h-screen lg:sticky lg:top-0 lg:left-0 bg-[#242424]
                         flex lg:flex-col flex-row justify-between items-center border-r border-[#2e2d2d]">
             {/* Top group => Profile pic, Dashboard Icon, To-Do List Icon, Kanban Board Icon */}
-            <div className="flex lg:flex-col flex-row justify-between items-center">
+            <div className="flex lg:flex-col flex-row justify-between items-center gap-5">
                 <div className="h-12 w-12 rounded-full cursor-pointer">
                     <img src={avatar} alt="PP" />
                 </div>
+                {Icons.slice(0,3).map((Icon, index) => (
+                    <div key={index} className="cursor-pointer hover:bg-[#2f2f2f] rounded-lg p-1">
+                        <Icon
+                            size={25}
+                            color={activeIcon === index ? '#fff' : '#a7a7a7'}
+                            onClick={() => setActiveIcon(index)}
+                        />
+                    </div>
+                ))}
             </div>
 
             {/* Bottom group => Notification Icon, More Icon */}
-            <div className="flex lg:flex-col flex-row justify-between items-center border border-amber-50 h-[150px] w-10 mb-5">
-
+            <div className="flex lg:flex-col flex-row justify-between items-center gap-5 mb-5">
+                {Icons.slice(3).map((Icon, index) => (
+                    <div key={index} className="cursor-pointer hover:bg-[#2f2f2f] rounded-lg p-1">
+                        <Icon
+                            size={25}
+                            color={activeIcon === (index + 3) ? '#fff' : '#a7a7a7'}
+                            onClick={() => setActiveIcon(index + 3)}
+                        />  
+                    </div>
+                ))}
             </div>
         </div>
     )
