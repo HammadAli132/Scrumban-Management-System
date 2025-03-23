693e64c454642dd48b34445e3a9336a33c6b6d38
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2025-03-23 17:34:30 +0500

    adding test routing

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
+const router = require('./routes');
+
 const PORT = process.env.PORT || 5000;
+
+app.use("/api/v1/", router);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+const express = require("express")
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Test Routing");
+})
+
+module.exports = router;
