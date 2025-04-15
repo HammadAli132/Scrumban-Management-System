a50919b6ec568eed7995648a9993cb3cb593f124
Daniyal Faraz <daniyalfaraz2003@gmail.com>
2025-04-16 00:46:36 +0500

    established connection to mongodb

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const {logger} = require('./middleware/logger');
+const { logger } = require('./middleware/logger');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
@@ -20,9 +20,15 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/v1/todos", todoRouter);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+	try {
+		mongoose.connect("mongodb://localhost:27017/scrumbandb");
+		console.log(`Connected to MongoDB`);
+		console.log(`Server is running at http://localhost:${PORT}`);
+	} catch (error) {
+		console.log(`Error connecting to MongoDB: ${error.message}`);
+	}
 });
