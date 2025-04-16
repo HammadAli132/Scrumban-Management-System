603430f38f96a9b7b194730532208b22df66be54
unknown <amnasiddiqui308@gmail.com>
2025-04-16 18:37:08 +0500

    delete to do list task controller added

--- Backend/controllers/todoListController.js
+++ Backend/controllers/todoListController.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
-const { getAllToDosByUserId, getCompletedTodosByUserId, getTrashedTodosByUserId, updateTodoById } = require('../services/todoListService.js');
+const { getAllToDosByUserId, getCompletedTodosByUserId, getTrashedTodosByUserId, updateTodoById, deleteTodoById } = require('../services/todoListService.js');
 
 const getAllToDoListTasks = async (req, res) => {
     try {
         // const userId = req.session.userId;
         const userId = req.params;
         const tasks = await getAllToDosByUserId(userId);
+
         res.status(200).json({success: true, data: tasks});
     } catch (error) {
         res.status(500).json({success: false, error: error.message});
@@ -17,6 +18,7 @@ const getCompletedToDoListTasks = async (req, res) => {
         // const userId = req.session.userId;
         const userId = req.params;
         const completedTasks = await getCompletedTodosByUserId(userId);
+
         res.status(200).json({success: true, data: completedTasks});
     } catch (error) {
         res.status(500).json({success: false, error: error.message});
@@ -28,6 +30,7 @@ const getTrashedToDoListTasks = async (req, res) => {
         // const userId = req.session.userId;
         const userId = req.params;
         const trashedTasks = await getTrashedTodosByUserId(userId);
+
         res.status(200).json({success: true, data: trashedTasks});
     } catch (error) {
         res.status(500).json({success: false, error: error.message});
@@ -54,9 +57,25 @@ const updateToDoListTask = async (req, res) => {
         }
 
         const updatedTask = await updateTodoById(id, updatedData);
+
         res.status(200).json({success: true, data: updatedTask});
     } catch (error) {
         res.status(500).json({success: false, error: error.message});
     }
 };
 
+const deleteToDoListTask = async (req, res) => {
+    try {
+        const {id} = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({ success: false, message: "Task not found!" });
+        }
+
+        const deletedTask = await deleteTodoById(id);
+
+        res.status(200).json({success: true, data: deletedTask});
+    } catch (error) {
+        res.status(500).json({success: false, error: error.message});
+    }
+}
