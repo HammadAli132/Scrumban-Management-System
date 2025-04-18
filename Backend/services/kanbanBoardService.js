47020d1bbe60bf9e30280841f735ca49bee6cde1
unknown <amnasiddiqui308@gmail.com>
2025-04-18 16:53:15 +0500

    service for getting kanban board implemented

--- Backend/services/kanbanBoardService.js
+++ Backend/services/kanbanBoardService.js
@@ -1,4 +1,19 @@
 const KanbanBoard = require("../models/kanbanBoard");
 const KanbanBoardTask = require("../models/kanbanBoardTask");
 
+const getKanbanBoardByProjectId = async (projectId) => {
+    try {
+        const kanbanBoard = await KanbanBoard.findOne({projectId: projectId});
+        const kanbanBoardId = kanbanBoard._id;
 
+        const boardTasks = await KanbanBoardTask.find({kanbanBoardId: kanbanBoardId});
+
+        if (!boardTasks) {
+            throw new Error("No tasks found for this kanban board");
+        }
+
+        return boardTasks;
+    } catch (error) {
+        throw new Error("Error getting kanban board: " + error.message);
+    }
+};
