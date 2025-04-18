ceb4c1a9cf880cf02edd3a21d5d041a8b8b8f0ff
unknown <amnasiddiqui308@gmail.com>
2025-04-18 18:39:32 +0500

    create kanban board task function added in controller

---
+++ Backend/controllers/kanbanBoardController.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const { createKanbanBoardTaskByProjectId, getKanbanBoardByProjectId } = require("../services/kanbanBoardService");
+
+const createKanbanBoardTask = async (req, res) => {
+    try {
+        const {projectId} = req.params;
+        const { 
+            title, 
+            description, 
+            priorityLevel, 
+            dueDate, 
+            swimLane, 
+            sprintId, 
+            userId 
+        } = req.body;
+
+        const taskData = {
+            ...(title != undefined && {title}),
+            ...(description != undefined && {description}),
+            ...(priorityLevel != undefined && {priorityLevel}),
+            ...(dueDate != undefined && {dueDate}),
+            ...(swimLane != undefined && {swimLane}),
+            ...(sprintId != undefined && {sprintId}),
+            ...(userId != undefined && {userId})
+        };
+
+        if (!mongoose.Types.ObjectId.isValid(projectId)) {
+            return res.status(404).json({ success: false, message: "Project not found!" });
+        } 
+
+        const newKanbanBoardTask = await createKanbanBoardTaskByProjectId(projectId, taskData);
+
+        res.status(200).json({success: true, data: newKanbanBoardTask});
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+};
+
+const getKanbanBoard = async (req, res) => {
+    try {
+        const {projectId} = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(projectId)) {
+            return res.status(404).json({ success: false, message: "Project not found!" });
+        } 
+
+        const kanbanBoardTasks = await getKanbanBoardByProjectId(projectId);
+
+        res.status(200).json({success: true, data: kanbanBoardTasks});
+    } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+    }
+};
