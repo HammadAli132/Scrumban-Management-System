c230231418488ccd93dac48611374335832792f6
unknown <amnasiddiqui308@gmail.com>
2025-04-18 16:25:57 +0500

    sprint model implemented

---
+++ Backend/models/sprint.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const sprintSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    startDate: {
+        type: Date
+    },
+    endDate: {
+        type: Date
+    },
+    retrospective: {
+        type: String,
+        trim: true
+    },
+    projectId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'project', 
+        required: true
+    }
+}, {timestamps});
+
+const Sprint = mongoose.model("Sprint", sprintSchema);
+
+module.exports = Sprint;
