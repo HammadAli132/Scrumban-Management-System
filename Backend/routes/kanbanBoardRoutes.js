ffb5678add23e8830b2cf4755beaf3f019633848
unknown <amnasiddiqui308@gmail.com>
2025-04-27 14:32:21 +0500

    get kanban id route added

--- Backend/routes/kanbanBoardRoutes.js
+++ Backend/routes/kanbanBoardRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { createKanbanBoardTask, getKanbanBoard, updateKanbanBoardTask, updateKanbanBoardTitle } = require("../controllers/kanbanBoardController");
+const { createKanbanBoardTask, getKanbanBoard, updateKanbanBoardTask, updateKanbanBoardTitle, getKanbanBoardId } = require("../controllers/kanbanBoardController");
 const router = express.Router();
 
 router.post("/:projectId", createKanbanBoardTask);
 router.get("/:projectId", getKanbanBoard);
 router.put("/boardtitle/:kanbanBoardId", updateKanbanBoardTitle);
 router.put("/kanbantask/:taskId", updateKanbanBoardTask);
+router.get("/kanbanId/:projectId", getKanbanBoardId);
